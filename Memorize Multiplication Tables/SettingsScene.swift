92ed22d3a30b409626a119f4e8168581795d3290
Greg Willis <gregorymwillis@gmail.com>
2016-03-09 00:17:36 -0700

    Added Multiplication number to settings scene

--- Memorize Multiplication Tables/SettingsScene.swift
+++ Memorize Multiplication Tables/SettingsScene.swift
@@ -12,11 +12,22 @@ import SpriteKit
 class SettingsScene: SKScene {
     
     var backButton: UIBarButtonItem!
+    var multiNumberSettingLabel: UILabel!
+    var multiNumberSettingTextField: UITextField!
+    
+    var multiNumberSetting: Int!
+    
     
     override func didMoveToView(view: SKView) {
         backgroundColor = Colors.darkRedColor
         spawnBackButton()
+        multiNumberSettingLabel = spawnMultiNumberSettingLabel()
+        multiNumberSettingTextField = spawnMultiNumberSettingTextField()
     }
+}
+
+// MARK: - Spawn functions
+extension SettingsScene {
     
     func spawnBackButton() {
         let backButton = UIBarButtonItem(title: "Back", style: .Plain, target: self, action: Selector("clickedDone:"))
@@ -29,15 +40,44 @@ class SettingsScene: SKScene {
         } else {
             assert(nil != GameViewController.controller, "GameViewController is nil")
         }
-        
     }
     
+    func spawnMultiNumberSettingLabel() -> UILabel {
+        multiNumberSettingLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 20))
+        if let multiNumberSettingLabel = multiNumberSettingLabel {
+            multiNumberSettingLabel.textColor = Colors.offWhiteColor
+            multiNumberSettingLabel.font = UIFont(name: "Futura", size: 20)
+            multiNumberSettingLabel.center = CGPoint(x: CGRectGetMaxX(self.frame) * 0.4, y: CGRectGetMaxY(self.frame) * 0.11)
+            multiNumberSettingLabel.textAlignment = .Left
+            multiNumberSettingLabel.text = "Set Multiplication Number"
+            view!.addSubview(multiNumberSettingLabel)
+        }
+        return multiNumberSettingLabel
+    }
+    
+    func spawnMultiNumberSettingTextField() -> UITextField {
+        multiNumberSettingTextField = UITextField(frame: CGRect(x: CGRectGetMaxX(self.frame) * 0.8, y: CGRectGetMaxY(self.frame) * 0.1, width: 40, height: 20))
+        if let multiNumberSettingTextField = multiNumberSettingTextField {
+            multiNumberSettingTextField.backgroundColor = Colors.offWhiteColor
+            view?.addSubview(multiNumberSettingTextField)
+        }
+
+        multiNumberSetting = Int(multiNumberSettingTextField.text!) ?? nil
+        return multiNumberSettingTextField
+    }
+}
+
+// MARK: - Helper functions
+extension SettingsScene {
     
     func clickedDone(sender: UIBarButtonItem) {
         transitionToGameScene()
     }
     
     func transitionToGameScene() {
+        multiNumberSettingLabel.removeFromSuperview()
+        multiNumberSettingTextField.removeFromSuperview()
+        
         if let skView = view, scene = TitleScene(fileNamed: "TitleScene") {
             skView.ignoresSiblingOrder = true
             scene.scaleMode = .ResizeFill
